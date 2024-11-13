bf37d32f11f786fdb635355e87693a2160dda776
Kyle Delaney <khdelaney99@gmail.com>
2024-11-13 13:28:02 -0500

    add more features to buttons in SubscriptionContainer

--- src/SubContainer/SubscriptionContainer.js
+++ src/SubContainer/SubscriptionContainer.js
@@ -1,16 +1,22 @@
 import './SubscriptionContainer.css'
+import { useNavigate } from 'react-router-dom';
 
 function SubscriptionContainer({ subData }) {
+  let navigate = useNavigate();
+  function renderSub(id) {
+    navigate(`/Subscription/${id}`);
+  }
   return (
     <section className='subscription-container'>
        <ul>
         {subData && subData.data ? (
           subData.data.map((sub) => (
-            <li key={sub.id}>
+            <button key={sub.id} onClick={() => renderSub(sub.id)}>
+              <p>{sub.attributes.active ? 'Active' : 'Inactive'}</p>
               <p>{sub.attributes.customer.attributes.first_name}'s Order</p>
               <img src={sub.attributes.tea.attributes.image} alt='Image of Tea'/>
               <p>{sub.attributes.tea.attributes.title}</p>
-            </li>
+            </button>
           ))
         ) : (
           <p>Loading...</p>
