9d2b9519917dff5e9c4701189523b1a501464035
Kyle Delaney <khdelaney99@gmail.com>
2024-11-13 18:46:57 -0500

    add styling to subscription container

--- src/SubContainer/SubscriptionContainer.js
+++ src/SubContainer/SubscriptionContainer.js
@@ -11,11 +11,11 @@ function SubscriptionContainer({ subData }) {
        <ul>
         {subData && subData.data ? (
           subData.data.map((sub) => (
-            <button key={sub.id} onClick={() => renderSub(sub.id)}>
-              <p>{sub.attributes.active ? 'Active' : 'Inactive'}</p>
-              <p>{sub.attributes.customer.attributes.first_name}'s Order</p>
-              <img src={sub.attributes.tea.attributes.image} alt='Image of Tea'/>
-              <p>{sub.attributes.tea.attributes.title}</p>
+            <button className='subscription-button' key={sub.id} onClick={() => renderSub(sub.id)}>
+              <p className="active-status">{sub.attributes.active ? 'Active' : 'Inactive'}</p>
+              <p className='customer'>{sub.attributes.customer.attributes.first_name}'s Order</p>
+              <img className='image' src={sub.attributes.tea.attributes.image} alt='Image of Tea'/>
+              <p className='tea-title'>{sub.attributes.tea.attributes.title}</p>
             </button>
           ))
         ) : (
